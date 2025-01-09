7c06610b31ce02687f3dc61c526c1efec1a56a49
Chicaofge <franciscocamposoliveira@alunos.esbn.pt>
2025-01-09 09:43:28 +0000

    Changes
    Changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -24,10 +24,12 @@ export default async function Home({}) {
           As Publicações mais recentes
         </p>
       </div>
+      </div>)}
+    
 
       {/* Lista de posts */}
       <ul className="divide-y divide-gray-200">
-        {allPosts.map((post=>{
+        {allPosts.map((post)=>{
           return (
             <li key={post.id} className="py-12">
             <article className="space-y-2 xl:grid xl:grid-colsl4 xl:space-y-0 xl:items-baseline">
@@ -45,12 +47,30 @@ export default async function Home({}) {
             </dl>
             <div className="space-y-5 xl:col-span-3">
               <div className="space-y-6">
-                <h2 className="text-2xl leading-8 font-bold tracking.tight"></h2>
+                <h2 className="text-2xl leading-8 font-bold tracking.tight">
+              < Link
+                href={`/posts/${post.slug}`}
+                className="text-gray-900"
+            >
+                {post.title}
+            </Link>
+            </h2>
+          { post.excerpt && (
+            <div className="prose max-w-none text-gray-500"> {post.excerpt}
+            </div>
+        )}
+            </div>
+            <div className="text-base leading-6 font-medium">
+              <Link
+                  href={`/posts/${post.slug}`}
+                  className="text-purple-500 hover: text-purple-600" aria-label={`Read "${post.title}"`}
+              >
+                  Ler Mais &rarr;
+              </Link>
               </div>
               </div>
-          
-        
-        </ul>"
-    </div>
-  )
-}
+              </article>
+              </li>
+      )
+    })}
+</ul>
