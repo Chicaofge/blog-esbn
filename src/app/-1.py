5f5261f06b007c7f15384ed8da4a86b1bd46a737
Chicaofge <franciscocamposoliveira@alunos.esbn.pt>
2025-01-14 13:55:21 +0000

    Create -1.py

---
+++ src/app/-1.py
@@ -0,0 +1,35 @@
+
+list = []
+
+def ler_numeros():
+    global list
+    list = []
+    for i in range(10):
+        num = int(input("Introduza um número inteiro: "))
+        list.append(num)
+
+def diferenca_max_min():
+    if len(list)==0:
+        return 0 
+    maior=max(list)
+    menor=min(list)
+    return maior-menor
+
+def contar_pares():
+    contar=0
+    for num in list:
+        if num % 2==0:
+            contar +=1
+    return contar
+
+def calcular_media():
+    if len(list)== :
+        return 0 
+    soma=sum(list)
+    media=soma/len(list)
+    return media
+ler_numeros()
+
+print("Diferença entre maior e menor:", diferenca_max_min())
+print("Quantidade de números pares:", contar_pares())
+print("Média dos números:", calcular_media())
